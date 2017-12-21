f13c930afda5d1fe4ee620821bcbcff1cbb7d3d6
DESKTOP-142R4IU\Aidan <expertsummer2002@gmail.com>
2017-12-21 13:15:48 -0600

    Creating an out perameter method

--- BattleTank/Source/BattleTank/Private/TankPlayerController1.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController1.cpp
@@ -34,7 +34,17 @@ void ATankPlayerController1::AimTowardsCrosshair()
 {
 	if (!GetControlledTank()) { return; }
 
-	// Get world location if linetrace through crosshair
-	// If it hits the landscape
-		// Tell Controlled tank to aim towards crosshair.
+	FVector HitLocation; // Out Perameter
+	if (GetSightRayHitLocation(HitLocation))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+			//  TODO Tell Controlled tank to aim towards crosshair.
+	}
+}
+
+// Get world location of linetrace through crosshair, true if hits landscape
+bool ATankPlayerController1::GetSightRayHitLocation(FVector& HitLocation) const
+{
+	HitLocation = FVector(1.0);
+	return true;
 }
