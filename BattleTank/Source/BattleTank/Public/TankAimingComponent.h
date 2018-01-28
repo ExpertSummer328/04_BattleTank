d7500574d77ae262289cf9bae2eba1924ac97bc2
DESKTOP-142R4IU\Aidan <expertsummer2002@gmail.com>
2018-01-28 11:01:21 -0600

    Attatching a Debugger to Unreal

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -29,7 +29,7 @@ public:
 	void AimAt(FVector HitLocation, float LaunchSpeed);
 
 	UPROPERTY(BlueprintReadOnly, Category = "State")
-	EFiringState FiringState = EFiringState::Reloading;
+	EFiringState FiringState = EFiringState::Aiming;
 	
 private:
 	// Sets default values for this component's properties
